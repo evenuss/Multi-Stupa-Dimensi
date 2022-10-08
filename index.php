25c4f920433474429260716402980a7230466656
Arjuna Efenus <nawatech@binus.edu>
2022-10-08 20:00:02 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,409 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="static/css/style1.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/fontawesome.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/brands.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/solid.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mantainence</title>
+</head>
+<body>
+  <div class="contactBar" id="contactBar" onclick="">
+    <div class="logo">
+      <img src="static/img/logo.png" alt="">
+        <a href="">
+          <b>PT MULTI STUPA DIMENSI</b></li>
+        </a>
+    </div>
+    <ul>
+      <li><a><i class="fa fa-phone"></i>Telepon: <b>(021) 29517622</b></a></li>
+      <li><a><i class="fa fa-message"></i>Email: <b>msdcons89@gmail.com</b></a></li>
+      <li><a><i class="fa fa-business-time"></i>Hari: <b>Senin-Jumat: 09.00 - 17.00</b></a></li>
+    </ul>
+    <!-- <a><i class="fa fa-phone"></i>Telepon: <b>(021) 29517622</b></a>
+    <a><i class="fa fa-message"></i>Email: <b>msdcons89@gmail.com</b></a>
+    <a><i class="fa fa-business-time"></i>Hari: <b>Senin-Jumat: 09.00 - 17.00</b></a> -->
+  </div>
+  <div class="navbar" id="navbar">
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="#">Profil</a></li>
+      <li><a href="#">Organisasi</a></li>
+      <li><a href="#">Bisnis</a></li>
+      <li><a href="#">Karir</a></li>
+      <li><a href="#">Kontak</a></li>
+    </ul>
+  </div>
+  <div class="slide-show">
+      <div class="mySlides fade">
+        <div class="numbertext">1 / 3</div>
+        <img src="static/img/slide1.jpg" style="width:100%">
+        <div class="text">
+          <h1>We Prepare <br>For The Future.</h1>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Quality Control System, 100% Statisfaction Guarantee</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Highly Professional Staff, Accurate Testing Process</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Unrivalled Workmanship, Professional and Qualified</p>
+          <a href="" class="ourservice-btn"><i class="fa fa-arrow-right"></i> Our Services</a> 
+        
+        </div>
+      </div>
+      
+      <div class="mySlides fade">
+        <div class="numbertext">2 / 3</div>
+        <img src="static/img/slide2.png" style="width:100%">
+        <div class="text">
+          <h1>We Prepare <br>For The Future.</h1>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Quality Control System, 100% Statisfaction Guarantee</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Highly Professional Staff, Accurate Testing Process</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Unrivalled Workmanship, Professional and Qualified</p>
+          <a href="" class="ourservice-btn"><i class="fa fa-arrow-right"></i> Our Services</a> 
+        
+        </div>
+
+      </div>
+      
+      <div class="mySlides fade">
+        <div class="numbertext">3 / 3</div>
+        <img src="static/img/slide3.jpg" style="width:100%">
+        <div class="text">
+          <h1>We Prepare <br>For The Future.</h1>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Quality Control System, 100% Statisfaction Guarantee</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Highly Professional Staff, Accurate Testing Process</p>
+          <p><i class="fa fa-check" style="background-color: rgb(255, 187, 0);border-radius:100%;padding: 2px;"></i> Unrivalled Workmanship, Professional and Qualified</p>
+          <a href="" class="ourservice-btn"><i class="fa fa-arrow-right"></i> Our Services</a> 
+          
+        </div>
+      </div>
+      
+      <!-- <a class="prev" onclick="plusSlides(-1)">❮</a> -->
+      <a class="prev" onclick="plusSlides(-1)"><i class="fa fa-arrow-left"></i></a>
+      <!-- <a class="next" onclick="plusSlides(1)">❯</a> -->
+      <a class="next" onclick="plusSlides(1)"><i class="fa fa-arrow-right"></i></a>
+
+      
+  </div>
+
+  
+  <div style="text-align:center;">
+    <span class="dot" onclick="currentSlide(1)"></span> 
+    <span class="dot" onclick="currentSlide(2)"></span> 
+    <span class="dot" onclick="currentSlide(3)"></span> 
+  </div>
+
+
+  <div class="about">
+      <h1 align="center">Tentang Kami</h1>
+
+    <div class="about-content">
+      <!-- <img src="static/img/logo.png" alt=""> -->
+      <p>
+        PT. MULTI STUPA DIMENSI adalah perusahaan
+        konsultan dengan bidang keahlian arsitektur,
+        teknik dan manajemen, MICE perusahaan ini
+        didirikan di Surabaya pada tahun 1989
+        berdasarkan Akte Pendirian Notaris Trining
+        Ariswati, SH Nomor: 72 tanggal 9 November
+        1989, dan Akte Perubahan Terakhir Notaris
+        Lyanawati, S.H., M.Kn. Nomor : 03 tanggal 15
+        Oktober 2021.
+        <!-- <br>
+        <br> -->
+        PT. MULTI STUPA DIMENSI didirikan oleh para
+        sarjana profesional yang mempunyai
+        pengalaman di bidang rekayasa teknik dan
+        manajemen terutama bidang Teknik Sipil,
+        Arsitektur, Perencanaan Wilayah/Kota dan
+        Kawasan, Mekanikal & Elektrikal, Manajemen
+        Konstruksi, Pemberdayaan Masyarakat, Teknik
+        Lingkungan.
+        <!-- <br>
+        <br> -->
+        PT. MULTI STUPA DIMENSI memiliki beberapa
+        pokok bidang dengan setiap bidangnya dipimpin
+        oleh seorang manajer.
+        BIDANG KONSTRUKSI :
+        Pekerjaan Sipil, Arsitektur, Perencanaan Tata
+        Ruang Tata Lingkungan.
+        BIDANG NON KONSTRUKSI :
+        Telematika Sistem Informasi, Jasa Survey, Jasa
+        Studi Penelitian dan Bantuan Teknik, Jasa
+        Konsultan Manajemen, Pengembangan
+        Pertanian dan Perdesaan, Pertambangan dan
+        Energi, Transportasi.
+
+      </p>
+  
+    </div>
+      
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <div class="service" align="center">
+    <h1 align="center">Services</h1>
+
+
+    <div class="service-card">
+      <img src="static/img/survey.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="service-card-desc">
+        <h4 align="">STUDI, PENELITIAN & SURVEY</h4>
+        <p>Studi Kelayakan, Studi
+          Pemasaran, Studi Keteknikan, Studi Lingkungan, Studi
+          Kemasyarakatan, Studi Pertanian, Pengukuran
+          topografi & pemetaan kadastral, Pengukuran
+          Hidrogeografi, Sistem Informasi Geografi,
+          Penyelidikan tanah (soil investigation), Studi dan
+          survey hidrologi, Survei pasar dan sosial – ekonomi</p>
+      </div>
+    </div>
+
+    <div class="service-card">
+      <img src="static/img/slide1.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="service-card-desc">
+      <h4 align="">PERENCANAAN & DESAIN</h4>
+      <p>Perencanaan Wilayah &
+        Perkotaan, Perencanaan Kawasan Perkotaan,
+        Rencana tata letak (Arsitektur & landscape),
+        Perencanaan jalan & Transportasi, Desain Irigasi dan
+        Drainase, Desain dan Pengembangan SIM dan
+        Networking, Pembuatan pra–rancangan, Pembuatan
+        gambar kerja, Perkiraan anggaran</p>
+
+      </div>
+    </div>
+
+    <div class="service-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="service-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="service-card">
+      <img src="static/img/pengembangan-sdm.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="service-card-desc">
+        <h4 align="">PENGEMBANGAN SDM & KELEMBAGAAN</h4>
+        <p>Pendidikan &
+          Pelatihan, Peningkatan Kapasitas & Pemberdayaan
+          Masyarakat, Prekrutan dan seleksi.</p>
+      </div>
+    </div>
+
+    <div class="service-card">
+      <img src="static/img/visual.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="service-card-desc">
+        <h4 align="">System Informasi Multimedia</h4>
+        <p>Sistem Informasi Management, Visualisasi
+          media, Publikasi, Animasi.</p>
+      </div>
+    </div>
+    
+  </div>
+
+
+
+
+  <div class="portfolio" align="center">
+
+    <h1>Portfolio</h1>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="portfolio-card">
+      <img src="static/img/project -supervision-and-management.jpg" alt="" style="width: 100%; height:200px;">
+      <div class="portfolio-card-desc">
+        <h4 align="">PENGAWASAN DAN MANAJEMEN PROYEK</h4>
+        <p>Pengawasan
+          lapangan, Pengendalian mutu, Manajemen konstruksi
+          Manajemen Pembangunan Perkotaan & Perdesaan.</p>
+      </div>
+    </div>
+
+    <div class="see-more-btn">
+      <a href="#portfolio">Lihat lebih banyak >></a>
+    </div>
+  </div>
+
+  <div class="client">
+    <h1 align="center">Client</h1>
+
+  
+    <div class="client-list">
+      <img src="static/img/metripekerjaanumum.jpg" alt="">
+      <img src="static/img/esdm.png" alt="">
+      <img src="static/img/bpn.png" alt="">
+      <img src="static/img/kemendagri.png" alt="">
+      <img src="static/img/lemhannas.png" alt="">
+      <img src="static/img/kemensos.png" alt="">
+      <img src="static/img/kemenhub.png" alt="">
+      <img src="static/img/kemenham.png" alt="">
+    
+
+    <div class="client-list">
+      <img src="static/img/jakartapng.png" alt="">
+      <img src="static/img/jabar.png" alt="">
+      <img src="static/img/surabaya.webp" alt="">
+      <img src="static/img/jawatimur.png" alt="">
+      <img src="static/img/serang.png" alt="">
+    </div>
+
+    <div class="client-list">
+      <img src="static/img/hutamakarya.png" alt="">
+      <img src="static/img/hutamainfrastruktur.png" alt="">
+      <img src="static/img/energitransporter.jpg" alt="">
+      <img src="static/img/jakartatollroad.jpg" alt="">
+    
+    </div>
+
+    <div class="client-list">
+      <img src="static/img/tirtasamarinda.jpg" alt="">
+      <img src="static/img/tirtapakuan.png" alt="">
+      <img src="static/img/pdampalopo.jpg_large" alt="">
+    
+    </div>
+    
+      
+
+  </div>
+
+
+  <div class="footer">
+    <div class="get-in-touch">
+      <h2>Let's discuss our how Multi Stupa Dimensi can help to transform your business and create impacts at scale!</h2>
+      <a href="" align="right">Hubungi Kami!</a>
+    </div>
+    <br>
+    <hr>
+    <div class="ft-navigation">
+      <h3>Navigation</h3>
+        <a href="index.html">Home</a>
+        <a href="">Profil</a>
+        <a href="">Organisasi</a>
+        <a href="">Bisnis</a>
+        <a href="">Karir</a>
+        <a href="">Kontak</a>
+    </div>
+  </div>
+
+  <p class="cp-right">&copy; Created by Vcode.Tech 2022</p>
+
+
+
+
+  
+  <script>
+    let slideIndex = 1;
+    showSlides(slideIndex);
+    
+    function plusSlides(n) {
+      showSlides(slideIndex += n);
+    }
+    
+    function currentSlide(n) {
+      showSlides(slideIndex = n);
+    }
+    
+    function showSlides(n) {
+      let i;
+      let slides = document.getElementsByClassName("mySlides");
+      let dots = document.getElementsByClassName("dot");
+      if (n > slides.length) {slideIndex = 1}    
+      if (n < 1) {slideIndex = slides.length}
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+      }
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].className += " active";
+    }
+
+    window.onscroll = function() {myFunction()};
+
+    function myFunction() {
+      if (document.documentElement.scrollTop > 50) {
+        document.getElementById("contactBar").hidden = true;
+        document.getElementById("navbar").style.position='Fixed';
+      }
+      if (document.documentElement.scrollTop < 100) {
+        document.getElementById("contactBar").hidden = false;
+       
+          document.getElementById("navbar").style.position='relative';
+
+
+      }
+  
+    }
+    </script>
+    
+  
+
+</body>
+</html>
---
+++ mantain.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="static/css/style.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/fontawesome.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/fontawesome.min.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/brands.css">
+    <link rel="stylesheet" href="static/css/fontawesome-free-6.2.0-web/fontawesome-free-6.2.0-web/css/solid.css">
+    <title>Mantainence</title>
+</head>
+<body>
+        <div class="navbar">
+            <img src="static/img/logo.png" style="padding-top:20px;" width="70" alt="">
+            <a href="">PT Multi Stupa Dimensi</a>
+        </div>
+        <div class="mantain" >
+            <h1>We're comming soon</h1>
+            <p>Selamat datang di Website PT Multi Stupa Dimensi, Website sedang dalam proses Mantainence, kami akan segera kembali!</p>
+            <br>
+            <div>
+                <br>
+                <p>Stay connected with Us!</p>
+                <div class="icon-page">
+                    <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+                    <a href=""><i class="fa-brands fa-instagram"></i></a>
+                    <a href=""><i class="fa-brands fa-linkedin-in"></i></a>
+                    <a href="msdcons89@gmail.com"><i class="fa-brands fa-google"></i></a>
+                </div>
+                
+            </div>
+        </div>
+        
+
+</body>
+</html>
